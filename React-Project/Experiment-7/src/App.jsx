47f778fe6cc38920fcb2896b56e9564845bad13a
shandilyasurya <suryanshshandilya1712@gmail.com>
2025-09-08 16:16:30 +0530

    Add files via upload

---
+++ React-Project/Experiment-7/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ProductsList from './productcard.jsx'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <ProductsList />
+  )
+}
+
+export default App
